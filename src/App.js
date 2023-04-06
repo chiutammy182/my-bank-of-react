40200c983ab2297c011560c8b371d0b66eedd9f3
tammyc182 <chiutammy182@gmail.com>
2023-04-06 14:52:22 -0400

    updated README.

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ class App extends Component {
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/bank-of-react-example-code-gh-pages">
+      <Router basename="/my-bank-of-react/">
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
